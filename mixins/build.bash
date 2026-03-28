39f67689db3f6774fe713ae149c52a9a9606b072
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2026-03-28 14:01:46 +0100

    monitoring: Generate yaml from mixins

--- mixins/build.bash
+++ mixins/build.bash
@@ -8,9 +8,14 @@ function build {
   mixin="$1"
   echo "Building $mixin"
   cd "$mixin" || true
-  mkdir -p "$OUTPUT_DIR"
+  mkdir -p build
   jb install
-  jsonnet -J vendor -e '(import "mixin.libsonnet")' -m "$OUTPUT_DIR"
+  jsonnet -J vendor -e '(import "mixin.libsonnet")' -m "build"
+  mkdir -p "$OUTPUT_DIR"
+  for file in build/*.json; do
+    YAML_FILE="$(basename "${file%.json}.yaml")"
+    yq "$file" -p json >"$OUTPUT_DIR/$YAML_FILE"
+  done
 }
 
 find ./mixins -maxdepth 1 -type d -name "*-mixin" | while read -r mixin; do
